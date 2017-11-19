da182b5e9c3901ad32f9a86267cc1437fae1e442
morblom <morblom@atex.com>
2017-11-19 12:14:10 +0100

    Added a proper license. FOrced updated onto 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'imfile'
 maintainer 'Atex Managed Services'
 maintainer_email 'managed-services@atex.com'
-license 'All Rights Reserved'
+license 'BSD-3-Clause'
 description 'Provides a resources that creates imfile configurations for Rsyslog'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
